d3bd1c926414fada82e25086b6ab7d20e1223558
Katie Gabriel <47282619+kgabriel113@users.noreply.github.com>
2022-03-18 01:06:39 -0700

    extract class

--- users/functions.py
+++ users/functions.py
@@ -1,12 +1,15 @@
 from .models import Profile, FriendRequest
 
-# friend_profile: Profile object to check friend request status against
-# requesting_user: user (ie from request.user)
-def get_friend_request_status(friend_profile, requesting_user):
-	if friend_profile not in requesting_user.profile.friends.all():
-		if len(FriendRequest.objects.filter(from_user=requesting_user).filter(to_user=friend_profile.user)) == 1:
-			return 'friend_request_sent'
-		if len(FriendRequest.objects.filter(from_user=friend_profile.user).filter(to_user=requesting_user)) == 1:
-			return 'friend_request_received'
-		return 'not_friend'
-	return 'none'
+
+class FriendRequestManager:
+
+    # friend_profile: Profile object to check friend request status against
+    # requesting_user: user (ie from request.user)
+    def get_friend_request_status(friend_profile: Profile, requesting_user: Profile.user):
+        if friend_profile not in requesting_user.profile.friends.all():
+            if len(FriendRequest.objects.filter(from_user=requesting_user).filter(to_user=friend_profile.user)) == 1:
+                return 'friend_request_sent'
+            if len(FriendRequest.objects.filter(from_user=friend_profile.user).filter(to_user=requesting_user)) == 1:
+                return 'friend_request_received'
+            return 'not_friend'
+        return 'none'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from cgi import test
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Profile
 from feed.models import Post
@@ -8,6 +9,7 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from .models import Profile, FriendRequest
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .functions import FriendRequestManager
 import random
 
 User = get_user_model()
@@ -99,8 +101,11 @@ def delete_friend(request, id):
 	friend_profile.friends.remove(user_profile)
 	return HttpResponseRedirect('/users/{}'.format(friend_profile.slug))
 
+
+
 @login_required
 def profile_view(request, slug):
+
 	p = Profile.objects.filter(slug=slug).first()
 	u = p.user
 	sent_friend_requests = FriendRequest.objects.filter(from_user=p.user)
@@ -108,22 +113,9 @@ def profile_view(request, slug):
 	user_posts = Post.objects.filter(user_name=u)
 
 	friends = p.friends.all()
-
 	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
-
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=p.user)) == 1:
-				button_status = 'friend_request_sent'
-
-		# if we have recieved a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
-
+	button_status = FriendRequestManager.get_friend_request_status(friend_profile=p, requesting_user=request.user)
+	
 	context = {
 		'u': u,
 		'button_status': button_status,
@@ -132,7 +124,6 @@ def profile_view(request, slug):
 		'rec_friend_requests': rec_friend_requests,
 		'post_count': user_posts.count
 	}
-
 	return render(request, "users/profile.html", context)
 
 def register(request):
@@ -176,18 +167,7 @@ def my_profile(request):
 	friends = p.friends.all()
 
 	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
-
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=you)) == 1:
-				button_status = 'friend_request_sent'
-
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
+	button_status = FriendRequestManager.get_friend_request_status(friend_profile=p, requesting_user=you)
 
 	context = {
 		'u': you,
