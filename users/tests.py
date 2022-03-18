3e173a9d0d682e42c261f1cccfd463a0cbbb8ddd
Katie Gabriel <47282619+kgabriel113@users.noreply.github.com>
2022-03-18 00:52:09 -0700

    add tests for my profile and user profile views

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth import get_user_model
+from .models import Profile
 
 # Create your tests here.
 
@@ -33,6 +35,26 @@ class RegistrationTestCase(TestCase):
         })
 
         self.assertEqual(response.status_code, 302)
+
+
+class ProfileViewTestCase(TestCase):   
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user('portcommunion', 'portcommunion@example.com', 'respondBloating291')
+        self.client = Client()
+        self.client.force_login(self.user)
+        
+    
+    def test_my_profile(self):
+        response = self.client.post('/my-profile/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_some_profile(self):
+        User = get_user_model()
+        test_user = User.objects.create_user('fakeuser2', 'a@b.com', 'fakepassword')
+
+        response = self.client.get(f'/users/{test_user.profile.slug}/')
+        self.assertEqual(response.status_code, 200)
 
     
         
